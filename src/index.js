906401bcc53bb3c6f7328390f423dd46ab54aa81
blazecolour <936x936@gmail.com>
2018-07-18 19:14:05 +0300

    fix parser renderer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import fs from 'fs';
 import path from 'path';
-import getParsers from './parsers';
+import getParser from './parser';
 import buildAst from './ast';
-import getRender from './render';
+import render from './renderer';
 
 const genDiff = (file1, file2) => {
   const data1 = fs.readFileSync(file1, 'utf8');
   const data2 = fs.readFileSync(file2, 'utf8');
   const fileExt = path.extname(file1);
-  const obj1 = getParsers(fileExt)(data1);
-  const obj2 = getParsers(fileExt)(data2);
+  const obj1 = getParser(fileExt)(data1);
+  const obj2 = getParser(fileExt)(data2);
   const ast = buildAst(obj1, obj2);
-  return getRender(ast);
+  return render(ast);
 };
 
 export default genDiff;
